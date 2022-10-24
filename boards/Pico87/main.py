5b080289dfa93825cb88bfb72c494332694a3be9
Hendrix <hendrix04@gmail.com>
2022-10-23 22:31:44 -0400

    update for osx

--- boards/Pico87/main.py
+++ boards/Pico87/main.py
@@ -2,7 +2,6 @@ import board
 
 from kb import KMKKeyboard
 
-<<<<<<< HEAD
 # Remove this if you need to push up for some reason
 from kb_passwords import Passwords
 
@@ -10,24 +9,13 @@ from kmk.consts import UnicodeMode
 from kmk.extensions.lock_status import LockStatus
 from kmk.extensions.stringy_keymaps import StringyKeymaps
 from kmk.handlers.sequences import send_string, unicode_string_sequence
-=======
-from kmk.extensions.lock_status import LockStatus
-from kmk.extensions.stringy_keymaps import StringyKeymaps
-from kmk.keys import KC
-from kmk.modules.layers import Layers
-from kmk.extensions.lock_status import LockStatus
-from kmk.extensions.stringy_keymaps import StringyKeymaps
->>>>>>> origin/master
 from kmk.keys import KC
 from kmk.modules.layers import Layers
 
 Pico87 = KMKKeyboard()
-<<<<<<< HEAD
 pswd = Passwords()
 
-Pico87.unicode_mode = UnicodeMode.WINALT
-=======
->>>>>>> origin/master
+Pico87.unicode_mode = UnicodeMode.OSX;
 
 class LEDLockStatus(LockStatus):
     def set_lock_leds(self):
@@ -47,13 +35,6 @@ class LEDLockStatus(LockStatus):
         if self.report_updated:
             self.set_lock_leds()
 
-<<<<<<< HEAD
-=======
-    if self.get_scroll_lock():
-        Pico87.leds.set_brightness(50, leds=[1])
-    else:
-        Pico87.leds.set_brightness(0, leds=[1])
->>>>>>> origin/master
 
 Pico87.modules.append(Layers())
 Pico87.extensions.append(LEDLockStatus())
@@ -77,15 +58,15 @@ Pico87.keymap = [[
     'TAB', ______,    'Q',    'W',    'E',    'R',    'T',    'Y',    'U',    'I',    'O',    'P', 'LBRC', 'RBRC', 'BSLS',  'DEL',  'END', 'PGDN',
    'CAPS', ______,    'A',    'S',    'D',    'F',    'G',    'H',    'J',    'K',    'L', 'SCLN', 'QUOT',  'ENT', ______, ______, ______, ______,
    ______, 'LSFT',    'Z',    'X',    'C',    'V',    'B',    'N',    'M', 'COMM',  'DOT', 'SLSH', ______, 'RSFT', ______, ______,   'UP', ______,
-   'LCTL', 'LGUI', ______, 'LALT', ______, ______,  'SPC', ______, ______, ______, 'RALT', 'RGUI', ______,  MOLYR, 'RCTL', 'LEFT', 'DOWN', 'RGHT',
+   'LCTL', 'LALT', ______, 'LGUI', ______, ______,  'SPC', ______, ______, ______, 'RGUI', 'RALT', ______, ______,  MOLYR, 'RCTL', 'LEFT', 'DOWN', 'RGHT',
 ], [
   # Layer 1
-    'ESC', ______,   'F1',   'F2',   'F3',   'F4', ______,   'F5',   'F6',   'F7',   'F8',   'F9',  'F10',  'F11',  'F12', 'PSCR', 'SLCK', 'PAUS',
-    'GRV',   'N1',   'N2',   'N3',   'N4',   'N5',   'N6',   'N7',   'N8',   'N9',   'N0', 'MINS',  'EQL', ______, 'BSPC',  'INS', 'HOME', 'PGUP',
-    'TAB', ______,    'Q',    'W',    'E',    'R',    'T',    'Y',    'U',    'I',    'O',    'P', 'LBRC', 'RBRC', 'BSLS',  'DEL',  'END', 'PGDN',
+    'ESC', ______,   'F1',   'F2',   'F3',   'F4', ______,   'F5',   'F6',   'F7',   'F8',   'F9',  'F10',  'F11',  'F12', 'PSCR', 'SLCK', 'MUTE',
+    'GRV',   'N1',   'N2',   'N3',   'N4',   'N5',   'N6',   'N7',   'N8',   'N9',   'N0', 'MINS',  'EQL', ______, 'BSPC', 'MPLY', 'HOME', 'VOLU',
+    'TAB', ______,    'Q',    'W',    'E',    'R',    'T',    'Y',    'U',    'I',    'O',    'P', 'LBRC', 'RBRC', 'BSLS', 'MRWD', 'MFFD', 'VOLD',
    'CAPS', ______,    'A',    'S',    'D',    'F',    'G',    'H',    'J',    'K',    'L', 'SCLN', 'QUOT',  'ENT', ______, ______, ______, ______,
    ______, 'LSFT',    'Z',    'X',    'C',    'V',    'B',    'N',    'M', 'COMM',  'DOT', PASSWD, ______,   SHRG, ______, ______,   'UP', ______,
-   'LCTL', 'LGUI', ______, 'LALT', ______, ______,  'SPC', ______, ______, ______, 'RALT', 'RGUI', ______, 'TRNS', 'RCTL', 'LEFT', 'DOWN', 'RGHT',
+   'LCTL', 'LALT', ______, 'LGUI', ______, ______,  'SPC', ______, ______, ______, 'RGUI', 'RALT', ______, 'TRNS', 'RCTL', 'LEFT', 'DOWN', 'RGHT',
 ]]
 
 if __name__ == '__main__':
